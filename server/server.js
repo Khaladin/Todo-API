ef224466f87203935d0fe9b36a037bc4fc8c67e8
Cory Hoban <hobancor@gmail.com>
2018-01-05 14:09:44 -0500

    troubleshooting

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 var port = process.env.PORT || 3000;
