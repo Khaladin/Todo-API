bc1d70cfd86b0ac1c9e7b5621cf0521a754aadf8
Cory Hoban <hobancor@gmail.com>
2018-01-05 17:07:56 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,26 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	//validate the id if not valid return 404
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Not a valid ID');
+	}
+	//remove todo by id
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send('No todos available');
+		}
+		res.status(200).send(todo);
+	}).catch((e) => res.status(400).send());
+		// success
+			//if no doc, send 404
+			// if doc, send doc back with 200
+		// error
+			// send 400 with empty body
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
